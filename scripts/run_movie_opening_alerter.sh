c1d5ddfcf5cace83a81804cd4b1c783aa78b0dca
phoenikx <nik211012@gmail.com>
2019-04-04 00:15:48 +0530

    Any output from script sends an email, removing all echos from there

--- scripts/run_movie_opening_alerter.sh
+++ scripts/run_movie_opening_alerter.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-echo "movie_name: $1"
-echo "location: $2"
-echo "movie_id: $3"
-echo "emails: $4"
-echo "movie_dates: $5"
 DATE=`date +%Y_%m_%d`;
 source $HOME/.virtualenvs/projects/movie_sorted/bin/activate && cd $HOME/projects/movie_sorted && . export_envs.sh && python3 movie_opening_alerter/movie_opening_alerter.py $1 $2 $3 $4 $5 >> ../logs/movie_opening_alerter/$DATE-$2.log
 
